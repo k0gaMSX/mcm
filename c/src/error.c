46adeb5859403647b84b40beb64d65778e8a0dff
roberto <roberto@0dbce89e-daeb-4827-a970-f95bd1b37987>
2009-11-20 21:05:00 +0000

    Deleting tabs and trailing white spaces
    git-svn-id: svn+ssh://localhost/var/local/svn/mcm@13 0dbce89e-daeb-4827-a970-f95bd1b37987

--- c/src/error.c
+++ c/src/error.c
@@ -15,43 +15,43 @@ static jmp_buf recover;
 int ierror()
   {
   errorg=0;
-  setjmp(recover); 
+  setjmp(recover);
   }
- 	
+
 
 
 int rerror (char nerror,char serr,char par1)
   {
     errorg=1;
-    
+
     switch (nerror)
       {
-      case EMEMORY :	
-	fprintf(stderr,"%s:%d: Internal memory error\n",filen,nline);	
-	exit(-1);               
+      case EMEMORY :
+        fprintf(stderr,"%s:%d: Internal memory error\n",filen,nline);
+        exit(-1);
       case ESYNTAX :
-	syntaxerror(filen,nline,serr);   
+        syntaxerror(filen,nline,serr);
         flushin();
-	longjmp(recover,1);     
+        longjmp(recover,1);
       case ELEX :
-	lexerror(filen,nline,par1);
-	syntaxerror(filen,nline,0);
+        lexerror(filen,nline,par1);
+        syntaxerror(filen,nline,0);
         flushin();
-	longjmp(recover,1);
+        longjmp(recover,1);
       case EEOF :
-	fprintf(stderr,"%s:%d: Found EOF before write sentence\n",filen,nline);
-	exit(-1);          
+        fprintf(stderr,"%s:%d: Found EOF before write sentence\n",filen,nline);
+        exit(-1);
       case EFILE:
-	fprintf(stderr,"%s:%d: It can't be written output file\n",filen,nline);
-	exit(-1);
+        fprintf(stderr,"%s:%d: It can't be written output file\n",filen,nline);
+        exit(-1);
       case EERROR:
-	fprintf(stderr,"%s:%d: No output file written due to finding errors\n",filen,nline);
-	exit(-1);
+        fprintf(stderr,"%s:%d: No output file written due to finding errors\n",filen,nline);
+        exit(-1);
       case E_INV_PAR:
-	errorsem(nerror,serr,par1,nline,filen);
-	longjmp(recover,1);     
+        errorsem(nerror,serr,par1,nline,filen);
+        longjmp(recover,1);
       }
-  
+
   }
 
 
