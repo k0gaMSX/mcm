c4428e684934d2408400e437111529339ed1f131
roberto <roberto@0dbce89e-daeb-4827-a970-f95bd1b37987>
2009-11-22 14:43:07 +0000

    Fixing bug in error recorery and changing -1 for EXIT_FAILURE
    git-svn-id: svn+ssh://localhost/var/local/svn/mcm@23 0dbce89e-daeb-4827-a970-f95bd1b37987

--- c/src/error.c
+++ c/src/error.c
@@ -1,13 +1,15 @@
 #define __ERRORC__
 #include <stdio.h>
+#include <stdlib.h>
 #include <setjmp.h>
 #include "error.h"
+#include "syntax.h"
 
 
 
 extern int nline;
 extern char *  filen;
-static jmp_buf recover;
+jmp_buf recover;
 
 
 
@@ -15,7 +17,6 @@ static jmp_buf recover;
 int ierror()
   {
   errorg=0;
-  setjmp(recover);
   }
 
 
@@ -28,25 +29,25 @@ int rerror (char nerror,char serr,char par1)
       {
       case EMEMORY :
         fprintf(stderr,"%s:%d: Internal memory error\n",filen,nline);
-        exit(-1);
+        exit(EXIT_FAILURE);
       case ESYNTAX :
         syntaxerror(filen,nline,serr);
         flushin();
         longjmp(recover,1);
       case ELEX :
-        lexerror(filen,nline,par1);
-        syntaxerror(filen,nline,0);
         flushin();
+        lexerror(filen,nline,par1);
+        syntaxerror(filen, nline, E_LEX);
         longjmp(recover,1);
       case EEOF :
         fprintf(stderr,"%s:%d: Found EOF before write sentence\n",filen,nline);
-        exit(-1);
+        exit(EXIT_FAILURE);
       case EFILE:
         fprintf(stderr,"%s:%d: It can't be written output file\n",filen,nline);
-        exit(-1);
+        exit(EXIT_FAILURE);
       case EERROR:
         fprintf(stderr,"%s:%d: No output file written due to finding errors\n",filen,nline);
-        exit(-1);
+        exit(EXIT_FAILURE);
       case E_INV_PAR:
         errorsem(nerror,serr,par1,nline,filen);
         longjmp(recover,1);
